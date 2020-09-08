60b80d40f3db244d6493b666a19a0eab6abac8b9
jagannathbehera <beherajagannath106@gmal.com>
2020-09-08 06:50:34 +0530

    Checking

---
+++ BookStore/Controller/BookController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BookStore.Models;
+using BookStore.Repository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookStore.Controllers
+{
+    public class BookController : Controller
+    {
+        private readonly BookRepository _bookRepository = null;
+
+        public BookController()
+        {
+            _bookRepository = new BookRepository();
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public ViewResult GetAllBooks()
+        {
+           var data= _bookRepository.GetAllBooks();
+
+            return View(data);
+        }
+        public ViewResult GetBookId(int id)
+        {
+            var data= _bookRepository.GetBookId(id);
+
+            return View(data);
+        }
+        public ViewResult AddNewBook()
+        {
+            return View();
+
+
+        }
+    }
+}
---
+++ BookStore/Repository/BookRepository.cs
@@ -0,0 +1,29 @@
+﻿using BookStore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStore.Repository
+{
+    public class BookRepository
+    {
+        public List<BookModels> GetAllBooks()
+        {
+            return DataSource();
+        }
+        public BookModels GetBookId(int id)
+        {
+            return DataSource().Where(x => x.Id == id).FirstOrDefault();
+        }
+        public List<BookModels> DataSource()
+        {
+            return new List<BookModels>()
+            {
+                new BookModels{Id=1,Title="MVC",Author="Jagannath",Description="This is MVC Book",Category="Programing",Language="English",TotalPages=107},
+                new BookModels{Id=1,Title="C#",Author="Chinyu",Description="This is C# Book",Category="Programing",Language="English",TotalPages=107},
+                new BookModels{Id=1,Title="English",Author="Behera",Description="This is English Book",Category="Programing",Language="English",TotalPages=107},
+            };
+        }
+    }
+}
